3bae00e49aad6f5a2c96ccd656cf113f85b908a4
veejask-41 <sajeevk2001@gmail.com>
2023-03-11 14:13:35 +0530

    created share copy functionality

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -10,12 +10,14 @@ import SwiperCore, {
   Navigation,
   Pagination,
 } from "swiper";
+import { FaShare } from "react-icons/fa";
 import "swiper/css/bundle";
 
 export default function Listing() {
   const params = useParams();
   const [listing, setListing] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [shareLingCopied, setShareLinkCopied] = useState(false);
   SwiperCore.use([Autoplay, Navigation, Pagination]);
 
   useEffect(() => {
@@ -54,6 +56,21 @@ export default function Listing() {
             </SwiperSlide>
           ))}
         </Swiper>
+        <div
+          className="fixed top-[13%] right-[3%] z-10 bg-white cursor-pointer border-2 border-gray-400 rounded-full w-12 h-12 flex justify-center items-center"
+          onClick={() => {
+            navigator.clipboard.writeText(window.location.href);
+            setShareLinkCopied(true);
+            setTimeout(() => {
+              setShareLinkCopied(false);
+            }, 2000);
+          }}
+        >
+          <FaShare className="text-lg text-slate-500" />
+        </div>
+        {shareLingCopied && (
+          <p className="fixed top-[23%] right-[5%] font-semibold border-2 border-gray-400 rounded-md bg-white z-10 p-2">Link copied</p>
+        )}
       </main>
     );
   }
